3ce41bcfe401890ce5990ac495f528f2c9852bf6
DESKTOP-E4UMQUR\bzic1 <bzic111@yandex.ru>
2022-04-05 13:10:52 +0300

    Добавьте файлы проекта.

---
+++ MetricsManagerHW/Controllers/CPUMetricsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetricsManagerHW.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CPUMetricsController : ControllerBase
+{
+    public CPUMetricsController()
+    {
+    }
+
+    #region Read
+
+    [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
+    public IActionResult GetCPUMetricsFromAgent([FromRoute] int agentId, [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+    {
+        return Ok();
+    }
+
+    [HttpGet("api/metrics/cpu/from/{fromTime}/to/{toTime}/percentiles/{percentile}")]
+    public IActionResult GetCPUMetricsPercentileFromAllCluster([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+    {
+        return Ok();
+    }
+
+    [HttpGet("cluster/from/{fromTime}/to/{toTime}")]
+    public IActionResult GetCPUMetricsFromAllCluster([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+    {
+        return Ok();
+    }
+    [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}/percentiles/{percentile}")]
+    public IActionResult GetCPUMetricsPercentileFromAgent([FromRoute] int agentId, [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+    {
+        return Ok();
+    }
+
+    #endregion
+}
---
+++ MetricsManagerHW/ext/AgentInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace MetricsManagerHW.ext;
+public class AgentInfo
+{
+    public int AgentId { get; }
+    public Uri AgentAddress { get; }
+    public AgentInfo(int id, Uri uri)
+    {
+        AgentId = id;
+        AgentAddress = uri;
+    }
+}
